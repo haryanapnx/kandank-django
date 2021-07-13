5a6deeb776ea3857113ccfe7aa4552a3991f95d0
haryfun <hary.vai@gmail.com>
2021-07-13 22:54:28 +0700

    fix: crud tutorials

--- tutorials/views.py
+++ tutorials/views.py
@@ -21,14 +21,39 @@ def tutorial_list(request):
         tutorials_serializer = TutorialsSerializer(tutorials, many=True)
         return JsonResponse(tutorials_serializer.data, safe=False)
 
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialsSerializer(data=tutorial_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'POST', 'DELETE'])
-def tutorial_detail(request):
-    print(request.method)
-    return JsonResponse(TutorialsSerializer.data, status=status.HTTP_200_OK)
+def tutorial_detail(request, pk):
+    tutorial = Tutorial.objects.get(pk=pk)
+    if request.method == 'GET':
+        tutorial_serializer = TutorialsSerializer(tutorial)
+        return JsonResponse(tutorial_serializer.data)
+
+    elif request.method == 'PUT':
+        request_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialsSerializer(tutorial, data=request_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data)
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        tutorial.delete()
+        return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET'])
 def tutorial_list_published(request):
-    print(request.method)
-    return JsonResponse(TutorialsSerializer.data, status=status.HTTP_200_OK)
+    tutorials = Tutorial.objects.filter(published=True)
+
+    if request.method == 'GET':
+        tutorials_serializer = TutorialsSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
